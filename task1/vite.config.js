0439020d55b506917b7de64739c317533b27d810
osmancalisir <osman.calisir@hotmail.com>
2025-07-03 18:18:14 +0200

    Treat Js files as JSX plugin added

--- task1/vite.config.js
+++ task1/vite.config.js
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, transformWithEsbuild } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null;
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        });
+      },
+    },
     react({
       include: /\.(js|jsx)$/,
-      jsxRuntime: 'classic'
     })
   ],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js?$/,
-    exclude: [],
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
   },
   test: {
     environment: 'happy-dom',
